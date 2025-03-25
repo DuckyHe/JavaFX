49017cd06107e99c493f75327b28392327dc86a2
DuckyHe <mohamed.boulakhras26@gmail.com>
2025-03-25 14:25:29 +0100

    question 2 et 3 fini

--- src/main/java/vue/PremiereApplication.java
+++ src/main/java/vue/PremiereApplication.java
@@ -1,15 +1,22 @@
 package vue;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class PremiereApplication extends Application {
 
     public void start(Stage stage)   {
+        VBox root = new VBox(10);
+        Scene scene = new Scene(root, 300, 80);
+        stage.setScene(scene);
         stage.setTitle("Hello JavaFX");
-        stage.setWidth(400);
-        stage.setHeight(150);
-        stage.centerOnScreen();
         stage.show();
+        Label labelHello = new Label("Hello");
+        root.getChildren().add(labelHello);
+        Label labelHelloBis = new Label("Hello JavaFX");
+        root.getChildren().add(labelHelloBis);
     }
 
     public static void main(String[] args) {
